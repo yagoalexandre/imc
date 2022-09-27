6ce7fc9874d48a6e2b2ad4b43a456426c9364969
yagoalexandre <yagoalexandreura@gmail.com>
2022-09-27 01:21:56 -0300

    imc funcionando, porém falta implementações

---
+++ imc/imc.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>HTML Forms</h2>
+
+<form name="imc" method="get" action="imc_teste_3.php">
+  <label for="peso">Seu peso:</label><br>
+  <input type="text" id="peso" name="peso" value=""><br>
+  
+  <label for="altura">Sua altura:</label><br>
+  <input type="text" id="altura" name="altura" value=""><br><br>
+  
+  <input type="submit" value="Checar">
+</form> 
+
+
+
+</body>
+</html>
+
--- imc/imc_teste_3.php
+++ imc/imc_teste_3.php
@@ -1,7 +1,7 @@
 <?php
     
-    $altura = $_GET ['altura'];
-    $peso = $_GET ['peso'];
+    $altura = $_GET['altura'];
+    $peso = $_GET['peso'];
     
     $calc = $peso/pow($altura,2) ; // faz o calculo do imc
     $imc = (number_format($calc,1)); // limite e arredonda as casas decimais para o IMC
